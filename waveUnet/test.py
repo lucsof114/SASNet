5cad028cee7d93248f5d2f6e7a2003b5e97678d4
lucas soffer <lsoffer@stanford.edu>
2020-11-15 23:09:10 -0800

    test

--- waveUnet/test.py
+++ waveUnet/test.py
@@ -1,4 +1,4 @@
-import sounddevice as sd
+
 import sys 
 import torch
 from params import params
@@ -11,11 +11,18 @@ def get_song(idx):
 	mxPath = "../data/DSD100/Mixtures/Dev/"
 	srcPath = "../data/DSD100/Sources/Dev/"
 	filename = os.listdir(mxPath)[idx]
+	svpath = "test/" + filename
+	if not os.path.isdir(svpath):
+		os.mkdir("test/" + filename)
 	songs = {}
 	songs["mix"] = np.mean(wavfile.read(mxPath + filename + "/mixture.wav")[1], axis = 1)
+	trck = songs["mix"]/np.max(np.abs(songs["mix"]))
+	wavfile.write(svpath + "/mix.wav", params["fs"], trck)
 	for inst in ["bass", "drums", "vocals", "other"]: 
-		songs[inst] = np.mean(wavfile.read(srcPath + filename + "/" + inst + ".wav")[1], axis = 1)
-	return songs
+		songs[inst] = wavfile.read(srcPath + filename + "/" + inst + ".wav")[1]
+		trck = np.mean(songs[inst]/np.max(np.abs(songs[inst])), axis = 1)
+		wavfile.write(svpath + "/" + inst + ".wav",  params["fs"], trck)
+	return songs, svpath
 
 
 
@@ -34,43 +41,42 @@ def estimate_tracks(model, song):
 
 	return torch.cat(ypred, axis = 1)
 
-def test_song(useModel, model_name, song_ind):
-	tracks = get_song(song_ind)
+def test_song(model_name, song_ind):
+	tracks, svpath = get_song(song_ind)
+	instrument = ["bass", "drums", "vocals", "other"]
+	model = WaveUNet()
+	model.load_state_dict(torch.load("save/" + model_name + "/" + model_name +".pkl"))
+	with torch.no_grad():
+		ypred = estimate_tracks(model, tracks["mix"])
+		for i in range(4): 
+			ypred[i] /= torch.max(ypred[i])
+			wavfile.write(svpath + "/estimate_" + instrument[i] + ".wav", params["fs"], ypred[i].numpy())
 
-	if useModel: 
-		model = WaveUNet()
-		model.load_state_dict(torch.load("save/" + model_name + "/" + model_name +".pkl"))
-		with torch.no_grad():
-			ypred = estimate_tracks(model, tracks["mix"])
-	else:
-		ypred = []
-		for inst in ["bass", "drums", "vocals", "other"]: 
-			ypred.append(tracks[inst])
-		ypred = np.vstack(ypred)
-	while True:
-		user_input = input ("choose track [mix/bass/drums/vocals/other/quit]")
-		if user_input == "quit":
-			return 
-		if user_input == "bass":
-			sd.play(ypred[0,:], blocking = True)
-		elif user_input == "drums":
-			sd.play(ypred[1, :], blocking = True)
-		elif user_input == "vocals":
-			sd.play(ypred[2, :], blocking = True)
-		elif user_input == "other":
-			sd.play(ypred[3, :], blocking = True)
-		elif user_input == "mix":
-			sd.play(tracks["mix"],  blocking = True)
-		else:
-			print("not valid input")
+
+
+	# while True:
+	# 	user_input = input ("choose track [mix/bass/drums/vocals/other/quit]")
+	# 	if user_input == "quit":
+	# 		return 
+	# 	if user_input == "bass":
+	# 		sd.play(ypred[0,:], blocking = True)
+	# 	elif user_input == "drums":
+	# 		sd.play(ypred[1, :], blocking = True)
+	# 	elif user_input == "vocals":
+	# 		sd.play(ypred[2, :], blocking = True)
+	# 	elif user_input == "other":
+	# 		sd.play(ypred[3, :], blocking = True)
+	# 	elif user_input == "mix":
+	# 		sd.play(tracks["mix"],  blocking = True)
+	# 	else:
+	# 		print("not valid input")
 
 
 
 if __name__ == '__main__':
-	if len(sys.argv) != 4: 
+	if len(sys.argv) != 3: 
 		print("Please add the following input arguments: ")
-		print("1. For model generated output add -m flag else add -t")
-		print("2. Model Name")
-		print("3. Song Index (number between 0 and 50)")
+		print("1. Model Name")
+		print("2. Song Index (number between 0 and 50)")
 
-	test_song(sys.argv[1] == "-m", sys.argv[2], int(sys.argv[3]))
+	test_song(sys.argv[1], int(sys.argv[2]))
