c017568b1090b40107a31c754817551824ae81b1
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2020-11-18 01:20:52 -0500

    Batch testing for waveunet

--- waveUnet/test.py
+++ waveUnet/test.py
@@ -1,4 +1,3 @@
-
 import sys 
 import torch
 from params import params
@@ -7,14 +6,13 @@ import os
 from wave_u_net import WaveUNet
 import numpy as np
 from utils import *
+import json
 
-def get_song(idx): 
+def get_song(model_name, idx): 
 	mxPath = "../data/DSD100/Mixtures/Dev/"
 	srcPath = "../data/DSD100/Sources/Dev/"
 	filename = os.listdir(mxPath)[idx]
-	svpath = "test/" + filename
-	if not os.path.isdir(svpath):
-		os.mkdir("test/" + filename)
+	svpath = "test/{}/{}".format(model_name, filename)
 	songs = {}
 	track = wavfile.read(mxPath + filename + "/mixture.wav")[1][::2]
 	songs["mix"] = track / np.max(np.abs(track))
@@ -27,13 +25,9 @@ def get_song(idx):
 		print(inst, songs[inst].shape)
 		numpy2wav(np.mean(songs[inst], axis=1).reshape(1, -1), [inst], svpath)
 
-	return songs, svpath
-
-
+	return songs, svpath, filename
 
 def estimate_tracks(model, song): 
-	song = torch.from_numpy(np.mean(song, axis=1)).float()
-	song = song.view(1, -1)
 	print(song.shape)
 	N = song.shape[1]
 	ypred = []
@@ -43,22 +37,40 @@ def estimate_tracks(model, song):
 		ypred.append(segment[0])
 	return torch.cat(ypred, axis = 1)
 
-def test_song(model_name, song_ind):
-	tracks, svpath = get_song(song_ind)
+def test_song(model_name, song_ind, metrics):
+	tracks, svpath, song_name = get_song(model_name, song_ind)
 	instrument = ["bass", "drums", "vocals", "other"]
 	device, gpu_ids = get_available_devices()
 	print(device)
 	model = WaveUNet()
 	model.load_state_dict(torch.load("save/" + model_name + "/" + model_name +".pkl", map_location=device))
 	with torch.no_grad():
-		ypred = estimate_tracks(model, tracks["mix"])
+		Ydata = torch.stack([tracks[instr] for instr in instrument], axis = 0)
+		Xdata = torch.sum(Ydata, axis = 0, keepdims = True)
+		ypred = estimate_tracks(model, Xdata)
+		loss = torch.mean((ypred - Ydata)**2).item()
+		print(song_name, "has loss", loss)
+		if metrics:
+			metrics[song_name] = loss
 		torch2wav(ypred, ["predicted_" + x for x in instrument], svpath)
-
 
 if __name__ == '__main__':
 	if len(sys.argv) != 3: 
 		print("Please add the following input arguments: ")
 		print("1. Model Name")
-		print("2. Song Index (number between 0 and 50)")
+		print("2. Either: all, many, or Song Index (number between 0 and 50)")
+		# All: 50 songs, Many: 10 songs, otherwise uses given song index
 
-	test_song(sys.argv[1], int(sys.argv[2]))
+	if not os.path.isdir("test/" + str(sys.argv[1]) + "/"):
+		os.mkdir("test/" + str(sys.argv[1]) + "/")
+
+	metrics = {}
+	if sys.argv[2] == "all":
+		for i in range(50):
+			test_song(sys.argv[1], i, metrics)
+	elif sys.argv[2] == "many":
+		for i in range(5):
+			test_song(sys.argv[1], i, metrics)
+	else:
+		metrics = None
+		test_song(sys.argv[1], int(sys.argv[2]), metrics)
